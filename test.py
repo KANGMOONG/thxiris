fe7823c42a5eedb5ee57d2b3e9946e16b5f72159
KANGMOONG <gilminkyun@gmail.com>
2025-09-25 13:28:59 +0900

    원복

--- test.py
+++ test.py
@@ -1,28 +1,29 @@
-import os
-from dotenv import load_dotenv
-import google.generativeai as genai
+from iris import ChatContext, Bot
+from iris.bot.models import ErrorContext
+from bots.gemini import get_gemini
+from bots.pyeval import python_eval, real_eval
+from bots.replyphoto import reply_photo
+from bots.text2image import draw_text
+from bots.coin import get_coin_info
 
-# .env 파일 로드
-load_dotenv()
+from iris.decorators import *
 
-# 환경 변수에서 API 키 가져오기
-API_KEY = os.getenv("GEMINI_API_KEY")
 
-if not API_KEY:
-    raise ValueError("GEMINI_API_KEY 환경변수가 설정되어 있지 않습니다. .env 파일을 확인하세요.")
+import sys, threading
 
-# API 키 설정
-genai.configure(api_key=API_KEY)
+iris_url = sys.argv[1]
+bot = Bot(iris_url)
 
-# 모델 선택
-model = genai.GenerativeModel("gemini-1.5-flash")
+@bot.on_event("message")
+@is_not_banned
+def on_message(chat: ChatContext):
+    #excel(chat)
+    try:
+        match chat.message.command:
+            
+            case "!hhi":
+                chat.reply(f"Hello {chat.sender.name}")
+            
+    except Exception as e :
+        print(e)
 
-# 테스트 프롬프트
-prompt = "안녕! 지금 이 응답은 Gemini API를 통해 받은거야?"
-
-# API 호출
-response = model.generate_content(prompt)
-
-# 결과 출력
-print("프롬프트:", prompt)
-print("응답:", response.text)
