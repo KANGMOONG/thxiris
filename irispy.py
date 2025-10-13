14a706d8614934eb1b11cc41e4c63d2db94acc13
KANGMOONG <gilminkyun@gmail.com>
2025-10-13 23:59:01 +0900

    test

--- irispy.py
+++ irispy.py
@@ -82,8 +82,8 @@ def on_message(chat: ChatContext):
             case "!코인" | "!내코인" | "!바낸" | "!김프" | "!달러" | "!코인등록" | "!코인삭제" | "!즐찾등록" | "!즐찾삭제":
                 get_coin_info(chat)
 
-            case "!즐찾등록" | "!즐찾삭제":
-                favorite_coin_info(chat)
+            #case "!즐찾등록" | "!즐찾삭제":
+             #   favorite_coin_info(chat)
 
             case "!1":
                 get_img(chat)
