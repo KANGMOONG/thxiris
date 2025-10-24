bee1841baa5c3c5f22d426e2da270c7f91997e6c
KANGMOONG <gilminkyun@gmail.com>
2025-10-24 22:25:25 +0900

    tets

---
+++ test_chat.py
@@ -0,0 +1,10 @@
+from iris import ChatContext, Bot
+from iris.bot.models import ErrorContext
+
+iris_url = sys.argv[1]
+bot = Bot(iris_url)
+
+@bot.on_event("message")
+@is_not_banned
+def on_message(chat: ChatContext):
+    print(chat.message.msg)
