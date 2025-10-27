afceefe0a191d36d2cfd7dbfa6eda649390b1330
KANGMOONG <gilminkyun@gmail.com>
2025-10-27 21:11:45 +0900

    test

--- irispy.py
+++ irispy.py
@@ -29,7 +29,7 @@ bot = Bot(iris_url)
 @bot.on_event("message")
 @is_not_banned
 def on_message(chat: ChatContext):
-    if chat_user_id == '6677876040401202432':
+    if chat.user_id == '6677876040401202432':
         chat.reply(chat.message.msg)
     
     try:
